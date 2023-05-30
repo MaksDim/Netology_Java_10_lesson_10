9c00f56b5869b7d5f2a2d18b86748a9bbbbdb421
Максимов Дмитрий <demasig@yandex.ru>
2023-05-30 10:21:50 +0400

    first commit

---
+++ src/test/java/ru/netology/domain/constructor/ConditionerTest.java
@@ -0,0 +1,26 @@
+package ru.netology.domain.constructor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConditionerTest {
+
+    @Test
+    public void shouldUseConstructor() {
+        Conditioner conditioner =
+                new Conditioner(1,
+                        "Holod3000",
+                        32,
+                        36,
+                        28,
+                        true);
+
+        Conditioner conditioner1 = new Conditioner();
+
+        Conditioner conditioner2 = new Conditioner(19,true);
+
+        assertEquals(32, conditioner.getMaxTemperature());
+    }
+
+}
---
+++ src/test/java/ru/netology/domain/field/ConditionerTest.java
@@ -0,0 +1,17 @@
+package ru.netology.domain.field;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConditionerTest {
+    @Test
+    public void shouldInitFields() {
+        Conditioner conditioner = new Conditioner();
+
+        assertEquals(10, conditioner.getMinTemperature());
+        //assertEquals(false, conditioner.isOn());
+        assertFalse(conditioner.isOn());
+        //assertTrue(conditioner.isOn());
+    }
+}
